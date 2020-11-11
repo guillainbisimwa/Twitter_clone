ba76e1e4915e96b980e5a90e410c1643d1c2c1ec
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-11-11 22:25:15 +0200

    Fix bugs

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,13 @@
-class RegistrationsController < Device::RegistrationsController
+class RegistrationsController < Devise::RegistrationsController
+
 
     private
 
-    def sign_up_params 
+    def sign_up_params
         params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
     end
 
-    def acount_update_params 
+    def account_update_params
         params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
     end
-    
+end
